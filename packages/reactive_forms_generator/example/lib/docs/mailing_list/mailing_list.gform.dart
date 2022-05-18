b62b315ed696fc8d121ade2d32f191b0cd57896a
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2022-05-18 14:45:55 +0200

    fix

---
+++ packages/reactive_forms_generator/example/lib/docs/mailing_list/mailing_list.gform.dart
@@ -0,0 +1,386 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file:
+
+part of 'mailing_list.dart';
+
+// **************************************************************************
+// ReactiveFormsGenerator
+// **************************************************************************
+
+class ReactiveMailingListFormConsumer extends StatelessWidget {
+  const ReactiveMailingListFormConsumer(
+      {Key? key, required this.builder, this.child})
+      : super(key: key);
+
+  final Widget? child;
+
+  final Widget Function(
+      BuildContext context, MailingListForm formModel, Widget? child) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    final formModel = ReactiveMailingListForm.of(context);
+
+    if (formModel is! MailingListForm) {
+      throw FormControlParentNotFoundException(this);
+    }
+    return builder(context, formModel, child);
+  }
+}
+
+class MailingListFormInheritedStreamer extends InheritedStreamer<dynamic> {
+  const MailingListFormInheritedStreamer(
+      {Key? key,
+      required this.form,
+      required Stream<dynamic> stream,
+      required Widget child})
+      : super(stream, child, key: key);
+
+  final MailingListForm form;
+}
+
+class ReactiveMailingListForm extends StatelessWidget {
+  const ReactiveMailingListForm(
+      {Key? key, required this.form, required this.child, this.onWillPop})
+      : super(key: key);
+
+  final Widget child;
+
+  final MailingListForm form;
+
+  final WillPopCallback? onWillPop;
+
+  static MailingListForm? of(BuildContext context, {bool listen = true}) {
+    if (listen) {
+      return context
+          .dependOnInheritedWidgetOfExactType<
+              MailingListFormInheritedStreamer>()
+          ?.form;
+    }
+
+    final element = context.getElementForInheritedWidgetOfExactType<
+        MailingListFormInheritedStreamer>();
+    return element == null
+        ? null
+        : (element.widget as MailingListFormInheritedStreamer).form;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MailingListFormInheritedStreamer(
+      form: form,
+      stream: form.form.statusChanged,
+      child: WillPopScope(
+        onWillPop: onWillPop,
+        child: child,
+      ),
+    );
+  }
+}
+
+class MailingListFormBuilder extends StatefulWidget {
+  const MailingListFormBuilder(
+      {Key? key, this.model, this.child, this.onWillPop, required this.builder})
+      : super(key: key);
+
+  final MailingList? model;
+
+  final Widget? child;
+
+  final WillPopCallback? onWillPop;
+
+  final Widget Function(
+      BuildContext context, MailingListForm formModel, Widget? child) builder;
+
+  @override
+  _MailingListFormBuilderState createState() => _MailingListFormBuilderState();
+}
+
+class _MailingListFormBuilderState extends State<MailingListFormBuilder> {
+  late FormGroup _form;
+
+  late MailingListForm _formModel;
+
+  @override
+  void initState() {
+    _form = FormGroup({});
+    _formModel = MailingListForm(widget.model, _form, null);
+
+    _form.addAll(_formModel.formElements().controls);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ReactiveMailingListForm(
+      form: _formModel,
+      onWillPop: widget.onWillPop,
+      child: ReactiveFormBuilder(
+        form: () => _form,
+        onWillPop: widget.onWillPop,
+        builder: (BuildContext context, FormGroup formGroup, Widget? child) =>
+            widget.builder(context, _formModel, widget.child),
+        child: widget.child,
+      ),
+    );
+  }
+}
+
+class MailingListForm implements FormModel<MailingList> {
+  MailingListForm(this.mailingList, this.form, this.path) {}
+
+  static String emailListControlName = "emailList";
+
+  final MailingList? mailingList;
+
+  final FormGroup form;
+
+  final String? path;
+
+  String emailListControlPath() => pathBuilder(emailListControlName);
+  List<String?> get emailListValue =>
+      emailListControl.value?.whereType<String?>().toList() ?? [];
+  bool get containsEmailList {
+    try {
+      form.control(emailListControlPath());
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Object? get emailListErrors => emailListControl.errors;
+  void get emailListFocus => form.focus(emailListControlPath());
+  void emailListValueUpdate(List<String?> value,
+      {bool updateParent = true, bool emitEvent = true}) {
+    emailListControl.updateValue(value,
+        updateParent: updateParent, emitEvent: emitEvent);
+  }
+
+  void emailListValuePatch(List<String?> value,
+      {bool updateParent = true, bool emitEvent = true}) {
+    emailListControl.patchValue(value,
+        updateParent: updateParent, emitEvent: emitEvent);
+  }
+
+  void emailListValueReset(List<String?> value,
+          {bool updateParent = true,
+          bool emitEvent = true,
+          bool removeFocus = false,
+          bool? disabled}) =>
+      emailListControl.reset(
+          value: value, updateParent: updateParent, emitEvent: emitEvent);
+  FormArray<String> get emailListControl =>
+      form.control(emailListControlPath()) as FormArray<String>;
+  void addEmailListItem(String value,
+      {List<AsyncValidatorFunction>? asyncValidators,
+      List<ValidatorFunction>? validators,
+      int? asyncValidatorsDebounceTime,
+      bool? disabled,
+      ValidatorsApplyMode validatorsApplyMode = ValidatorsApplyMode.merge}) {
+    List<ValidatorFunction> resultingValidators = [
+      (control) => emailValidator(control as FormControl<String>)
+    ];
+    List<AsyncValidatorFunction> resultingAsyncValidators = [];
+
+    switch (validatorsApplyMode) {
+      case ValidatorsApplyMode.merge:
+        if (validators != null) {
+          resultingValidators.addAll(validators);
+        }
+        if (asyncValidators != null) {
+          resultingAsyncValidators.addAll(asyncValidators);
+        }
+        break;
+      case ValidatorsApplyMode.override:
+        if (validators != null) {
+          resultingValidators = validators;
+        }
+
+        if (asyncValidators != null) {
+          resultingAsyncValidators = asyncValidators;
+        }
+        break;
+    }
+
+    emailListControl.add(FormControl<String>(
+      value: value,
+      validators: resultingValidators,
+      asyncValidators: resultingAsyncValidators,
+      asyncValidatorsDebounceTime: asyncValidatorsDebounceTime ?? 250,
+      disabled: disabled ?? false,
+    ));
+  }
+
+  MailingList get model => MailingList(emailList: emailListValue);
+  void updateValue(MailingList value,
+          {bool updateParent = true, bool emitEvent = true}) =>
+      form.updateValue(
+          MailingListForm(value, FormGroup({}), null).formElements().rawValue,
+          updateParent: updateParent,
+          emitEvent: emitEvent);
+  void resetValue(MailingList value,
+          {bool updateParent = true, bool emitEvent = true}) =>
+      form.reset(
+          value: MailingListForm(value, FormGroup({}), null)
+              .formElements()
+              .rawValue,
+          updateParent: updateParent,
+          emitEvent: emitEvent);
+  void reset({bool updateParent = true, bool emitEvent = true}) => form.reset(
+      value: formElements().rawValue,
+      updateParent: updateParent,
+      emitEvent: emitEvent);
+  String pathBuilder(String? pathItem) =>
+      [path, pathItem].whereType<String>().join(".");
+  FormGroup formElements() => FormGroup({
+        emailListControlName: FormArray<String>(
+            mailingList?.emailList
+                    .map((e) => FormControl<String>(
+                          value: e,
+                          validators: [
+                            (control) =>
+                                emailValidator(control as FormControl<String>)
+                          ],
+                          asyncValidators: [],
+                          asyncValidatorsDebounceTime: 250,
+                          disabled: false,
+                        ))
+                    .toList() ??
+                [],
+            validators: [
+              (control) => mailingListValidator(control as FormArray<String>)
+            ],
+            asyncValidators: [],
+            asyncValidatorsDebounceTime: 250,
+            disabled: false)
+      },
+          validators: [],
+          asyncValidators: [],
+          asyncValidatorsDebounceTime: 250,
+          disabled: false);
+}
+
+class ReactiveMailingListFormArrayBuilder<T> extends StatelessWidget {
+  const ReactiveMailingListFormArrayBuilder(
+      {Key? key,
+      this.control,
+      this.formControl,
+      this.builder,
+      required this.itemBuilder})
+      : assert(control != null || formControl != null,
+            "You have to specify `control` or `formControl`!"),
+        super(key: key);
+
+  final FormArray<T>? formControl;
+
+  final FormArray<T>? Function(MailingListForm formModel)? control;
+
+  final Widget Function(BuildContext context, List<Widget> itemList,
+      MailingListForm formModel)? builder;
+
+  final Widget Function(
+          BuildContext context, int i, T? item, MailingListForm formModel)
+      itemBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    final formModel = ReactiveMailingListForm.of(context);
+
+    if (formModel == null) {
+      throw FormControlParentNotFoundException(this);
+    }
+
+    return ReactiveFormArray<T>(
+      formArray: formControl ?? control?.call(formModel),
+      builder: (context, formArray, child) {
+        final itemList = (formArray.value ?? [])
+            .asMap()
+            .map((i, item) {
+              return MapEntry(
+                i,
+                itemBuilder(
+                  context,
+                  i,
+                  item,
+                  formModel,
+                ),
+              );
+            })
+            .values
+            .toList();
+
+        return builder?.call(
+              context,
+              itemList,
+              formModel,
+            ) ??
+            Column(children: itemList);
+      },
+    );
+  }
+}
+
+class ReactiveMailingListFormFormGroupArrayBuilder<V> extends StatelessWidget {
+  const ReactiveMailingListFormFormGroupArrayBuilder(
+      {Key? key,
+      this.extended,
+      this.getExtended,
+      this.builder,
+      required this.itemBuilder})
+      : assert(extended != null || getExtended != null,
+            "You have to specify `control` or `formControl`!"),
+        super(key: key);
+
+  final ExtendedControl<List<Map<String, Object?>?>, List<V>>? extended;
+
+  final ExtendedControl<List<Map<String, Object?>?>, List<V>> Function(
+      MailingListForm formModel)? getExtended;
+
+  final Widget Function(BuildContext context, List<Widget> itemList,
+      MailingListForm formModel)? builder;
+
+  final Widget Function(
+          BuildContext context, int i, V? item, MailingListForm formModel)
+      itemBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    final formModel = ReactiveMailingListForm.of(context);
+
+    if (formModel == null) {
+      throw FormControlParentNotFoundException(this);
+    }
+
+    final value = (extended ?? getExtended?.call(formModel))!;
+
+    return StreamBuilder<List<Map<String, Object?>?>?>(
+      stream: value.control.valueChanges,
+      builder: (context, snapshot) {
+        final itemList = (value.value() ?? <V>[])
+            .asMap()
+            .map((i, item) => MapEntry(
+                  i,
+                  itemBuilder(
+                    context,
+                    i,
+                    item,
+                    formModel,
+                  ),
+                ))
+            .values
+            .toList();
+
+        return builder?.call(
+              context,
+              itemList,
+              formModel,
+            ) ??
+            Column(children: itemList);
+      },
+    );
+  }
+}
