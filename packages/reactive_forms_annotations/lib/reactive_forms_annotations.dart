9bd21f31d9a2b4dccd20595b7ee786703def0383
Vasiliy Ditsyak <vasilich6107@users.noreply.github.com>
2022-06-29 19:38:57 +0200

    reexport flutter widgets

--- packages/reactive_forms_annotations/lib/reactive_forms_annotations.dart
+++ packages/reactive_forms_annotations/lib/reactive_forms_annotations.dart
@@ -1,5 +1,6 @@
 library reactive_forms_annotations;
 
+export 'package:flutter/widgets.dart';
 export 'src/reactive_form_annotation.dart';
 export 'src/form_group_annotation.dart';
 export 'src/form_array_annotation.dart';
