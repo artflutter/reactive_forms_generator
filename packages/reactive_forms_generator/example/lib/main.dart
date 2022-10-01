7808914ac710a5bef7cfb4e16f8b95ca611b53a4
Benji <farquhar.benji@gmail.com>
2022-10-01 20:31:11 +1300

    Add generic demo

--- packages/reactive_forms_generator/example/lib/main.dart
+++ packages/reactive_forms_generator/example/lib/main.dart
@@ -1,42 +1,19 @@
 import 'package:example/docs/animated_url_list/url_list_form.dart';
+import 'package:example/docs/array_nullable/array_nullable_form.dart';
 import 'package:example/docs/delivery_list/delivery_point_route_form.dart';
 import 'package:example/docs/delivery_list/delivery_route_form.dart';
-import 'package:example/docs/mailing_list/mailing_list_form.dart';
 import 'package:example/docs/freezed/freezed_form.dart';
-import 'package:example/docs/user_profile/user_profile_form.dart';
-import 'package:example/docs/array_nullable/array_nullable_form.dart';
+import 'package:example/docs/generic/generic_form.dart';
 import 'package:example/docs/group/group_form.dart';
+import 'package:example/docs/login/login_form.dart';
 import 'package:example/docs/login_extended/login_extended_form.dart';
 import 'package:example/docs/login_extended_nullable/login_extended_nullable_form.dart';
-import 'package:example/docs/login/login_form.dart';
+import 'package:example/docs/mailing_list/mailing_list_form.dart';
+import 'package:example/docs/user_profile/user_profile_form.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class Routes {
-  static const loginExtended = '/login-extended';
-
-  static const login = '/login';
-
-  static const mailingList = '/mailing-list';
-
-  static const userProfile = '/user-profile';
-
-  static const group = '/group';
-
-  static const deliveryList = '/delivery-list';
-
-  static const deliveryPoint = '/delivery-point';
-
-  static const loginExtendedNullable = '/login-extended-nullable';
-
-  static const arrayNullable = '/array-nullable';
-
-  static const freezed = '/freezed';
-
-  static const animatedUrlList = '/animated-url-list';
 }
 
 class MyApp extends StatelessWidget {
@@ -62,9 +39,36 @@ class MyApp extends StatelessWidget {
         Routes.deliveryList: (_) => const DeliveryListFormWidget(),
         Routes.deliveryPoint: (_) => const DeliveryPointWidget(),
         Routes.freezed: (_) => const FreezedFormWidget(),
+        Routes.generic: (_) => const GenericFormWidget(),
         Routes.animatedUrlList: (_) => UrlListForm(),
       },
       home: const DeliveryListFormWidget(),
     );
   }
+}
+
+class Routes {
+  static const loginExtended = '/login-extended';
+
+  static const login = '/login';
+
+  static const mailingList = '/mailing-list';
+
+  static const userProfile = '/user-profile';
+
+  static const group = '/group';
+
+  static const deliveryList = '/delivery-list';
+
+  static const deliveryPoint = '/delivery-point';
+
+  static const loginExtendedNullable = '/login-extended-nullable';
+
+  static const arrayNullable = '/array-nullable';
+
+  static const freezed = '/freezed';
+
+  static const generic = '/generic';
+
+  static const animatedUrlList = '/animated-url-list';
 }
